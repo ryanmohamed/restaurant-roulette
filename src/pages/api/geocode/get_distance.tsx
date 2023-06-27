c7feb9e668a15014031f8632a4db918d826031b4
Ryan <reyaznyc@gmail.com>
2023-06-27 00:59:28 -0400

    production tests

--- src/pages/api/geocode/get_distance.tsx
+++ src/pages/api/geocode/get_distance.tsx
@@ -68,26 +68,30 @@ export default async function handler(
         const data = await response.json();
         console.log("before rows is known to be an array");
         if(data?.rows === undefined || data?.rows === null || typeof data.rows?.length !== "number" || data.rows.length === 0) throw new Error("Failed to retrieve distance information.");
-        
+      
+        console.log("before elements is defined");
         const { elements } = data.rows[0];
         if(elements === null || elements === undefined || typeof elements?.length !== "number" || elements.length === 0) throw new Error("Failed to retrieve distance information");
 
+        console.log("before distanceData is defined");
         const distanceData = elements[0];
         const { distance, duration, duration_in_traffic, origin, destination, status } = distanceData;
+        console.log("before traffic is defined");
         if (duration_in_traffic === undefined || duration_in_traffic === null) throw new Error("Failed to retrieve distance information");
         
         const seconds = (duration?.value || 0) + (duration_in_traffic?.value || 0);
         const hours = Math.floor(seconds / 3600);
         const remainingMinutes = Math.floor((seconds % 3600) / 60)
 
+        console.log("before new text is defined");
         const newText = `${hours > 0 ? hours+"h" : ""}${remainingMinutes} mins`
         const newDuration = {
             text: newText,
             value: seconds
         }
 
-        return res.status(400).json({ error: "Failed to retrieve distance information from Distance Matrix API." });
-        //return res.status(200).json( { distance, duration: newDuration } );
+        //return res.status(400).json({ error: "Failed to retrieve distance information from Distance Matrix API." });
+        return res.status(200).json( { distance, duration: newDuration } );
     }
 
     catch (error) {
