258bffc3dc863f550943d17aa6b5a11ded8fd8aa
Ryan Mohamed <41905250+ryanmohamed@users.noreply.github.com>
2023-06-28 14:08:55 -0400

    Update SidePanel.tsx

--- src/components/client/SidePanel.tsx
+++ src/components/client/SidePanel.tsx
@@ -6,8 +6,8 @@ export default function SidePanel() {
   const { location } = useLocationContext();
   const [ searchTerms, setSearchTerms ] = useState<string | null>(null);
   return (
-    <div className="md:flex-grow flex flex-col font-barlow md:w-2/6 lg:w-1/5 bg-stone-950 p-2 py-4 md:px-4 bt">
-      <div className="screen-h">
+    <div className="md:flex-grow flex flex-col font-barlow md:w-2/6 lg:w-1/5 bg-stone-950 bt">
+      <div className="md:screen-h p-2 py-4 md:px-4 ">
         <h3 className="text-2xl text-stone-300 border-b-2 border-zinc-700 pb-1">Modify your search</h3>  
         <FilterForm />
       </div>
